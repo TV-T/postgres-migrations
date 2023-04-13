c4b4529a7b7f9020080114ef593cfaafbb31096a
Timothy Vane-Tempest <tim@vane-tempest.net>
2023-04-13 15:22:11 +0300

    Need to create the dir.

--- copyScript.js
+++ copyScript.js
@@ -1,6 +1,6 @@
-const { copyFileSync } = require('fs');
+const { copyFileSync, mkdirSync } = require('fs');
 const { join } = require('path');
 
 const path = '/migrations/0_create-migrations-table.sql';
-
+mkdirSync(join(__dirname, 'dist/migrations'), { recursive: true });
 copyFileSync(join(__dirname, `src${path}`), join(__dirname, `dist${path}`));
